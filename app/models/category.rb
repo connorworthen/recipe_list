31d0a0da4cfb9c1377a472e2e62a656e53b512cd
connorworthen <connorw220@protonmail.com>
2020-07-20 13:05:35 -0700

    new migration and model

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,13 +4,15 @@ has_many :recipes
 end
 #naming conventions, creating new migrations, tables name, ini. objects
 
-# rails c
+# tux
 
 # category = Category.new
 
 # recipe = Category.new
 
 #@recipe = Recipe.create(name: "cheese", url: "www.cheese.com", category_id: category.id)
+
+# @recipe = Recipe.new(params)
 
 # easier way?
 
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,19 @@
 class Recipe < ActiveRecord::Base
   belongs_to :user
-  # belongs_to :category #category model
+  belongs_to :category #category model
+  has_many :ingredients
   has_many :bookmarks
 
   validates_presence_of :name, :url
   validates :name, uniqueness: true
 
 end
+
+
+#conventions, get a model and migration foreign key, active record, class names singular etc. snake case migration belongs/has_many connects db key,
+
+ # ingredient = Ingredient.create(name: "eggs", recipe_id: recipe.id) 
+ # created new migration file ingredients then made model class and added has_many to recipe in order to add the name of an ingredient
+ # to "fried rice" do ingredient = Recipe.new then recipe.ingredients.build(name: "rice") or 
+ # ingredient = Ingredient.create(name: "eggs", recipe_id: recipe.id)
+ # study line 13 and write down to bettr understand 
